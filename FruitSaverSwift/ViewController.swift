eb10dfbce9ac2cbd4ad7841ccf12a24d5825f184
Grant Kim <grantkim@Grants-MacBook-Pro.local>
2018-04-09 16:54:58 -0400

    camera set up

--- FruitSaverSwift/ViewController.swift
+++ FruitSaverSwift/ViewController.swift
@@ -7,18 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var fruitImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    // user chose an image --> do something with it
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            fruitImage.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Can't convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Request failed")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTapped(_ sender: Any) {
+        // presents camera to the user
+        present(imagePicker, animated: true, completion: nil)
     }
+    
 
 
 }
